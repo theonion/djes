ec42a3cfcb491b265c87160ed9dae0005552acb4
Chris Sinchok <csinchok@theonion.com>
2015-03-15 03:36:54 -0500

    Work on testing, bulk indexing, etc

--- tests/test_result.py
+++ tests/test_result.py
@@ -3,11 +3,11 @@ import pytest
 from model_mommy import mommy
 import time
 
-from example.app.models import SimpleObject
+from example.app.models import SimpleObject, RelatableObject
 
 
 @pytest.mark.django_db
-def test_get(es_client):
+def test_simple_get(es_client):
 
     management.call_command("sync_es")
 
@@ -20,3 +20,24 @@ def test_get(es_client):
     assert from_es.baz == test_object.baz
     assert from_es.__class__.__name__ == "SimpleObject_ElasticSearchResult"
     assert from_es.save is None
+
+    with pytest.raises(RelatableObject.DoesNotExist):
+        RelatableObject.search_objects.get(id=test_object.id)
+
+
+@pytest.mark.django_db
+def test_related_get(es_client):
+
+    management.call_command("sync_es")
+
+    test_object = mommy.make(RelatableObject)
+    time.sleep(1)  # Let the index refresh
+
+    from_es = RelatableObject.search_objects.get(id=test_object.id)
+    assert from_es.foo == test_object.foo
+    assert from_es.bar == test_object.bar
+    assert from_es.baz == test_object.baz
+    assert from_es.__class__.__name__ == "SimpleObject_ElasticSearchResult"
+    assert from_es.save is None
+
+
