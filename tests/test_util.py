0e00b4c18d7db159f29c707fc834872b8c7d0605
Mike Parent <mparent@umich.edu>
2015-09-30 14:08:42 -0500

    Test create/delete during batch query

--- tests/test_util.py
+++ tests/test_util.py
@@ -31,8 +31,37 @@ def test_count_batch_queries(es_client):
                                             (5, 3),
                                             (3, 5)]:
             reset_queries()
-            chunks = list(batched_queryset(SimpleObject.objects.all(), chunksize=chunksize))
-            assert objects == chunks
+            results = list(batched_queryset(SimpleObject.objects.all(), chunksize=chunksize))
+            assert objects == results
             assert len(connection.queries) == expected_queries
     finally:
         settings.DEBUG = False
+
+
+@pytest.mark.django_db
+def test_delete_during_query(es_client):
+    objects = mommy.make(SimpleObject, _quantity=10)
+    qs = batched_queryset(SimpleObject.objects.all(), chunksize=3)
+    results = [next(qs) for _ in range(3)]
+    # Delete during batched fetch
+    objects[3].delete()  # Not yet fetched
+    # Finish batched fetch
+    results.extend(list(qs))
+    # Querys
+    assert results == (objects[:3] + objects[4:])
+
+
+@pytest.mark.django_db
+def test_create_during_query(es_client):
+    objects = mommy.make(SimpleObject, _quantity=10)
+    qs = batched_queryset(SimpleObject.objects.all(), chunksize=3)
+    results = [next(qs) for _ in range(3)]
+    # Create more objects during batched fetch
+    new_objects = mommy.make(SimpleObject, _quantity=10)
+    # Finish batched fetch
+    results.extend(list(qs))
+
+    # Final chunk (based on initial object count 10) would normally be size 1. Number of chunks
+    # fetched is based on initial size, but last chunk fetched will grab enough new objects to fill
+    # chunk (size 3).
+    assert results == (objects + new_objects[:2])
