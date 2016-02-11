bcdf6dd09df29f0e6281b4ef41dc2acf6d68bc1d
Cameron Lowe <cam.m.lowe@gmail.com>
2016-02-11 10:57:03 -0600

    bin next results for efficiency.

--- djes/search.py
+++ djes/search.py
@@ -55,6 +55,7 @@ class LazySearch(Search):
     def __init__(self, *args, **kwargs):
         super(LazySearch, self).__init__(*args, **kwargs)
         self.position = kwargs.get("from", 0)
+        self._results = []
 
     def __len__(self):
         if 'size' in self._extra:
@@ -66,12 +67,15 @@ class LazySearch(Search):
 
     def next(self):
         # Tracks index on the search object so we can iterate across multiple queries.
-        start, end = self.position, self.position + 1
+        size = self._extra.get("size", 10)
+        from_ = self.position
         self.position += 1
-        try:
-            return self[start:end][0]
-        except IndexError:
-            raise StopIteration
+        if from_ + 1 > len(self._results):
+            try:
+                self._results += list(self.extra(from_=from_, size=size))
+            except IndexError:
+                raise StopIteration
+        return self._results[from_]
 
     def __getitem__(self, n):
         # TODO: Investigate a better way to iterate through the entire result set.
